2243bd438c58d637ad4f56f7309f83c88d23fa40
MoyTW <moytravis@gmail.com>
2017-04-16 10:57:20 -0700

    Add Tile+Board to gen & draw
    Doesn't currently have any blocks movement semantics

--- assets/js/component.js
+++ assets/js/component.js
@@ -7,7 +7,7 @@ var Component = {
   },
 
   PhaserSprite : function PhaserSprite (x, y, spriteName) {
-    this.sprite = game.add.sprite(x, y, spriteName);
+    this.sprite = game.add.sprite(x * 30, y * 30, spriteName);
     this.sprite.width = 30;
     this.sprite.height = 30;
   },
--- assets/js/game.js
+++ assets/js/game.js
@@ -1,24 +1,43 @@
 "use strict";
 
 var skiffEntity;
+var board;
 var manager = new ECS.EntityManager();
 
 var Game = {
 
   preload : function() {
+    game.load.image('white_square', './assets/images/white_square.png');
     game.load.image('skiff', './assets/images/skiff.png');
   },
 
   create: function () {
     menu.width = 650;
     menu.height = 400;
+
     skiffEntity = manager.createEntity();
     // Honestly, this is a little silly, isn't it? I mean, I know partials are
     // second-nature for lispy folks but javascript's this is apparently a pit
     // of vipers.
-    var SpriteComponent = Component.PhaserSprite.bind(null, 0, 0, 'skiff');
+    manager.addComponent(skiffEntity, Component.Position.bind(null, 5, 5));
+    var SpriteComponent = Component.PhaserSprite.bind(null,
+                                                      skiffEntity.position.x,
+                                                      skiffEntity.position.y,
+                                                      'skiff');
     manager.addComponent(skiffEntity, SpriteComponent);
-    manager.addComponent(skiffEntity, Component.Position.bind(null, 0, 0));
+
+
+    board = new Level.Board(15, 15);
+    // Phaser has a concept of tilesets, which we will want to use eventually!
+    for (var x = 0; x < board.width; x++) {
+      for (var y = 0; y < board.height; y++) {
+        if (board.tiles[x][y].blocked) {
+          var sprite = game.add.sprite(x, y, 'white_square');
+          sprite.x = x * 30;
+          sprite.y = y * 30;
+        }
+      }
+    }
   },
 
   update: function () {
