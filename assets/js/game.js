bacc1e130a3d4592184893d1895a31c7002b3e38
MoyTW <moytravis@gmail.com>
2017-06-01 19:46:35 -0700

    Add fns for rand selection by weight

--- assets/js/game.js
+++ assets/js/game.js
@@ -5,6 +5,29 @@ function randomInt(seededRand, min, max) {
   return Math.floor(seededRand() * (max - min + 1)) + min;
 }
 
+// weights must be a reducible data structure. Annoying the map iterators aren't
+// reducible by default in javascript (!?!?) so you can't actually call this on
+// the iterator provided by map.keys(). Really, js?
+function randomIndex(seededRand, weights) {
+  var selection = randomInt(seededRand, 0, weights.reduce((a, b) => a+b, 0));
+
+  var sum = 0;
+  var idx = 0;
+  for (var weight of weights) {
+    sum += weight;
+    if (selection <= sum) {
+      return idx;
+    }
+    idx++;
+  }
+}
+
+// weights must be a reducible data structure
+function randomSelection(seededRand, options, weights) {
+  var idx = randomIndex(seededRand, weights);
+  return options[idx];
+}
+
 var Game = {
 
   cursors : null,
